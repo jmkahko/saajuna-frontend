c50c2b5c96ea9c3a1cbaf0218b6560dfb9ad1822
Leena Kähkönen <leenakahkonen14@gmail.com>
2021-06-20 16:01:27 +0300

    havaintoaseman hakua ja saanyt.ts

--- src/app/havaintoasemat/havaintoasemat.component.ts
+++ src/app/havaintoasemat/havaintoasemat.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
 import { HavaintoasemaService } from '../havaintoasema.service';
 import { HavaintoAsemat } from '../havaintoasemat';
-import { Observable, Subject } from 'rxjs';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-havaintoasemat',
@@ -14,8 +11,6 @@ export class HavaintoasematComponent implements OnInit {
   // havaintoasemat$: Observable<HavaintoAsemat[]>;
   havaintoasemat: Array<HavaintoAsemat> = [];
   hasemat: Array<HavaintoAsemat> = [];
-
-  // private searchTerms = new Subject<string>();
 
   constructor(private havaintoAsemaService: HavaintoasemaService) {
     this.havaintoAsemaService
@@ -32,22 +27,4 @@ export class HavaintoasematComponent implements OnInit {
   }
 
   ngOnInit(): void {}
-  // haeHavaintoAsema(formdata) {}
-
-  // ngOnInit(): void {
-  //   this.havaintoasemat$ = this.searchTerms.pipe(
-  //     // wait 300ms after each keystroke before considering the term
-  //     // http-pyyntöjä tehdään korkeintaan 300 ms välein (odotusaika)
-  //     debounceTime(300),
-
-  //     // ignore new term if same as previous term
-  //     // ei tehdä uutta hakua jos termi ei ole muuttunut
-  //     distinctUntilChanged(),
-
-  //     // switch to new search observable each time the term changes
-  //     // vaihdaliitos vaihtaa palvelimelta tulevaan streamiin, jolla saadaan
-  //     // haetut sankarit.
-  //     switchMap((term: string) => this.havaintoAsemaService.haeHavaintoAsemat(term))
-  //   );
-  // }
 }
---
+++ src/app/saanyt.ts
@@ -0,0 +1,22 @@
+export class SaaNyt {
+  // konstruktorimetodi joka rakentaa säänyt-olion
+  constructor(
+    public _id: string,
+    public time: String, // Kellonaika
+    public t2m: Number, // Lämpötila
+    public ws_10min: Number, // Tuuli
+    public wg_10min: Number, //  Tuulen puuska
+    public wd_10min: Number, // Tuulen suunta
+    public rh: Number, // Kosteus
+    public td: Number, // Kastepiste
+    public r_1h: Number, // Sademäärä tunnissa
+    public ri_10min: Number, //
+    public snow_aws: Number, // Lumensyvyys
+    public p_sea: Number, // Paine
+    public vis: Number, // Näkyvyys
+    public n_man: Number, // Pilvisyys
+    public wawa: Number, // wawa
+    public fmisid: Number, // esim. Kajaani Petäisenniska 126736
+    public place: String
+  ) {}
+}
