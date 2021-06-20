1f6400503223ea81a09f61e7e013ef90d3d4e5bd
Leena Kähkönen <leenakahkonen14@gmail.com>
2021-06-20 12:41:22 +0300

    Havaintoasemien haku tietokannasta

--- src/app/havaintoasemat/havaintoasemat.component.ts
+++ src/app/havaintoasemat/havaintoasemat.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { HavaintoasemaService } from '../havaintoasema.service';
 import { HavaintoAsemat } from '../havaintoasemat';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-havaintoasemat',
@@ -9,14 +11,34 @@ import { HavaintoAsemat } from '../havaintoasemat';
   styleUrls: ['./havaintoasemat.component.css'],
 })
 export class HavaintoasematComponent implements OnInit {
-  havaintoasemat: Array<HavaintoAsemat> = [];
+  havaintoasemat$: Observable<HavaintoAsemat[]>;
+
+  private searchTerms = new Subject<string>();
 
   constructor(private havaintoAsemaService: HavaintoasemaService) {
-    this.havaintoAsemaService
-      .haeHavaintoAsemat()
-      .subscribe((data) => (this.havaintoasemat = data));
+    // this.havaintoAsemaService
+    //   .haeHavaintoAsemat()
+    //   .subscribe((data) => (this.havaintoasemat = data));
+  }
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.havaintoasemat$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      // http-pyyntöjä tehdään korkeintaan 300 ms välein (odotusaika)
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      // ei tehdä uutta hakua jos termi ei ole muuttunut
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      // vaihdaliitos vaihtaa palvelimelta tulevaan streamiin, jolla saadaan
+      // haetut sankarit.
+      switchMap((term: string) => this.havaintoAsemaService.haeHavaintoAsemat())
+    );
+  }
   haeHavaintoAsema(formdata) {}
 }
